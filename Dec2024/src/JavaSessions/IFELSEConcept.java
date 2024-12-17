c42757655253d3ca4985752a295a21894d3431dc
sathappan1989 <a.s.sathappan@hotmail.com>
2024-12-17 11:02:50 -0700

    ifElseConcept

--- Dec2024/src/JavaSessions/IFELSEConcept.java
+++ Dec2024/src/JavaSessions/IFELSEConcept.java
@@ -4,9 +4,11 @@ public class IFELSEConcept {
 
 	public static void main(String[] args) {
 		
-		// ==comparison operator to compare two primitive data types
-		// = assigment operator
-		// to compare two String(non primitive data types) -- we have to use .equals(
+		// == comparison operator to compare two primitive data types
+		// = assignment operator
+		// to compare two Strings(non primitive) data types -- we have to use .equals()
+		
+		System.out.println(10==10);
 		
 		int a = 10;
 		int b = 10;
