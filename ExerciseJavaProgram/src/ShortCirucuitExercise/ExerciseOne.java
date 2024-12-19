bddc4300c231cb23990041865ba1486d30afafac
sathappan1989 <a.s.sathappan@hotmail.com>
2024-12-18 19:47:14 -0700

    ShortCircuitExerciseProgram

---
+++ ExerciseJavaProgram/src/ShortCirucuitExercise/ExerciseOne.java
@@ -0,0 +1,17 @@
+package ShortCirucuitExercise;
+
+public class ExerciseOne {
+
+	public static void main(String[] args) {
+		int x=5;
+		int y=10;
+		if (x>0 && y>0 && x<y && x*2 < y) {
+			System.out.println("Hi");
+		}
+		else {
+			System.out.println("Bye"); //Output
+		}
+
+	}
+
+}
---
+++ ExerciseJavaProgram/src/ShortCirucuitExercise/ExerciseTwo.java
@@ -0,0 +1,11 @@
+package ShortCirucuitExercise;
+
+public class ExerciseTwo {
+
+	public static void main(String[] args) {
+		boolean a=false;
+			boolean b=false;
+				boolean result = a && b;
+					System.out.println(result);
+	}
+}
