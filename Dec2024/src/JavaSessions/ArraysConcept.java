a3e598d41f58e5fd5658b3dbed7a3eb969127793
sathappan1989 <a.s.sathappan@hotmail.com>
2024-12-23 21:36:05 -0700

    ArrayDayOne

---
+++ Dec2024/src/JavaSessions/ArraysConcept.java
@@ -0,0 +1,103 @@
+package JavaSessions;
+
+import java.util.Arrays;
+
+public class ArraysConcept {
+
+	public static void main(String[] args) {
+		//array:
+		//array : static array //Homogeneous (disadvantages)
+		//1. new keyword
+		//2. array literals
+		
+		//int array:
+		int i[]=new int[4]; //new keyword //16bytes storage (4*4=16 Bytes)
+		//length of the array = 3
+		
+		//i[-1]=500; //AIOB - Negative index not allowed in java
+		i[0]=100;
+		i[1]=200;
+		i[2]=300;
+		i[3]=400;
+		//i[4]=500; //AIOB //RunTime Error
+		
+		//System.out.println(i); //[I@2dda6444 {Address of the memory}
+		
+		System.out.println(Arrays.toString(i)); //Arrays - class
+		System.out.println("*********************************************");
+		System.out.println(i[0]);
+		System.out.println(i[3]);
+		//System.out.println(i[4]); // ArrayIndexOutOfBoundsException //AIOB
+		
+		System.out.println(i.length);
+		System.out.println("*********************************************");
+		int len = i.length;
+		System.out.println("Li = " + 0);
+		System.out.println("Hi = " + (len-1));
+		System.out.println("length = " + (len));
+		
+		System.out.println("*********************************************");
+		
+		
+		int p[]=new int[4];
+		p[0]=10;
+		p[2]=20;
+		p[3]=30;
+		System.out.println(p[1]); //0 Default value of integer will be 0(ZERO)
+		System.out.println(Arrays.toString(p));
+		
+		System.out.println("*********************************************");
+		
+		
+		//print all the value of the array, iterate the array : using for loop:
+		
+		int price[]=new int[4];
+		price[0]=100;
+		price[1]=200;
+		price[2]=300;
+		price[3]=400;
+		
+		System.out.println(Arrays.toString(price));
+		
+		for(int k=0;k<price.length;k++) {
+			System.out.println(price[k]);
+			if(price[k]==200) {
+				System.out.println("bye!!");
+				//break;
+			}
+		}
+		System.out.println("*********************************************");
+		//double array:
+		double d[]=new double[2]; //16 bytes (8*2 = 16bytes)
+		d[0]=12.33;
+		//d[1]=23.44;
+		System.out.println(Arrays.toString(d));
+		
+		//char array:
+		System.out.println("*********************************************");
+		char ch[]=new char[3]; //0-2
+//		ch[0]='a';
+//		ch[1]='b';
+//		ch[2]='c';
+		System.out.println(Arrays.toString(ch));
+		
+		
+		System.out.println("*********************************************");
+		//boolean array
+		boolean fg[]=new boolean[2];
+		//System.out.println(Arrays.toString(fg)); //Default value will be false
+		
+		fg[0]=true;
+		fg[1]=false;
+		System.out.println(Arrays.toString(fg));
+		/*Default value*/
+		//int,short, byte, long: 0
+		//double,float : 0
+		//boolean = false
+		//char = space 
+		
+		/*Use Case*/
+		//Gender, Month, Week
+	}
+
+}
--- Dec2024/src/JavaSessions/TimeComplexity.java
+++ Dec2024/src/JavaSessions/TimeComplexity.java
@@ -1,10 +1,23 @@
 package JavaSessions;
-/*Important for Interview*/
+/*Important for Interview*//*It is basis fundamental for all language*/ /*How exactly you have written the code*/ /*performance improvement*/
+//When you are increasing the data, need to check the performance of the application
+
+//50k-1
+//500k-1
+//5-1
+
 public class TimeComplexity {
 
 	public static void main(String[] args) {
 		
-
+		int i =10;
+		System.out.println(i);  // TC: Big Oh--> O(n) -- > Time Complexity 0(1)
+		//0(1)--constant time
+		
+		for(int p=1;p<=10;p++) {
+			System.out.println(p);
+		}
+		//
 	}
 
 }
