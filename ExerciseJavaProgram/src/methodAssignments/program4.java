3f2e2fead4ecbf1892eb215f4000a57d7dd51657
sathappan1989 <sathappanas@outlook.com>
2025-01-08 19:22:06 -0700

    MinandMaxNumber

--- ExerciseJavaProgram/src/methodAssignments/program4.java
+++ ExerciseJavaProgram/src/methodAssignments/program4.java
@@ -1,10 +1,39 @@
 package methodAssignments;
-/**/
+/*Define two methods to print the maximum and the minimum number respectively among three numbers entered by user.*/
 public class program4 {
 
+	public int max(int a,int b,int c) {
+		if(a>b && a>c) {
+			return a;
+		}
+		else if (b>c) {
+			return b;
+		}
+		else {
+			return c;
+		}	
+	}
+	
+	public int min(int a, int b,int c) {
+		if(a<b && a<c) {
+			return a;
+		}
+		else if (b<c) {
+			return b;
+		}
+		else {
+			return c;
+		}	
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		program4 p1=new program4();
+		int maximum=p1.max(2,15,7);
+		System.out.println(maximum);
+		
+		int minimum=p1.min(2,15,1);
+		System.out.println(minimum);
 	}
 
 }
