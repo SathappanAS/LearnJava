7646234294c7d4a5412cdfe427a30ea95b1feb2c
sathappan1989 <sathappanas@outlook.com>
2025-01-15 20:35:02 -0700

    test

--- ExerciseJavaProgram/src/EncapsulationAssignments/Person.java
+++ ExerciseJavaProgram/src/EncapsulationAssignments/Person.java
@@ -1,5 +1,27 @@
 package EncapsulationAssignments;
+/*Assignment 1:
 
+Objective: The objective of this assignment is to create a class that uses encapsulation to protect its data and 
+provide getter and setter methods for accessing the data.
+
+Instructions:
+Create a class called "Person" with the following private attributes: name (String), age (int), and gender (String).
+Create getter and setter methods for each attribute.
+Write a method called "printInfo" that prints out the name, age, and gender of the person.
+Create an instance of the "Person" class and set its attributes using the setter methods.
+Call the "printInfo" method to verify that the data was set correctly.
+
+Questions:
+What is encapsulation and how does it relate to object-oriented programming?
+Why is it important to use getter and setter methods instead of accessing attributes directly?
+How can encapsulation improve the security and reliability of a program?
+What is the difference between a private attribute and a public attribute?
+How does encapsulation help with code maintainability and scalability?
+
+ * 
+ * */
+
+/*https://martinfowler.com/tags/encapsulation.html*/
 public class Person {
 
 	private String name;
