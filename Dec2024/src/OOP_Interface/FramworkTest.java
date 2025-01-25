885c4438997aa73c06395020f5bb133ca9e8f6bd
sathappan1989 <sathappanas@outlook.com>
2025-01-24 22:30:10 -0700

    Interface

---
+++ Dec2024/src/OOP_Interface/FramworkTest.java
@@ -0,0 +1,15 @@
+package OOP_Interface;
+
+public class FramworkTest implements NaveenAutomationLabs{
+	
+	@Override
+	public void open() {
+		
+	}
+	
+	@Override
+	public void get() {
+			
+		}
+
+}
---
+++ Dec2024/src/OOP_Interface/NaveenAutomationLabs.java
@@ -0,0 +1,23 @@
+package OOP_Interface;
+
+public interface NaveenAutomationLabs {
+
+	default void open() {
+		
+	}
+	
+	default void get() {
+			
+		}
+	
+	default void close() {
+		
+	}
+	
+	default void titel() {
+		
+	}
+	
+	//Advantage of default keyword : We don;t want to implements all the dafult methods .. 
+	
+}
