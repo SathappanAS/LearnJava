3ac1f9ce5d90254f78b2b019c523b25683b0a72f
sathappan1989 <sathappanas@outlook.com>
2025-01-29 13:52:17 -0700

    finallyKeyword

--- Dec2024/src/ExceptionHandling/FinallyBlock.java
+++ Dec2024/src/ExceptionHandling/FinallyBlock.java
@@ -1,10 +1,24 @@
 package ExceptionHandling;
-
+/*Doesn't matter exception is coming or not
+ * it must be executed
+ * */
 public class FinallyBlock {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		
+		System.out.println("A");
+		System.out.println("A");
+		System.out.println("A");
+		
+		try {
+			int i=9/0;
+		}
+		catch(ArithmeticException e) {
+			System.out.println("AE is coming...");
+			e.printStackTrace();
+		}
+		finally{
+			System.out.println("finally block");
+		}
+	}	
 }
