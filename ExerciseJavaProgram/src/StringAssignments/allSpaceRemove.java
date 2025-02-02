345a2e12d788fa09108ddfe02248ceba7b19a6f7
sathappan1989 <sathappanas@outlook.com>
2025-02-01 22:28:45 -0700

    String Replace

---
+++ ExerciseJavaProgram/src/StringAssignments/allSpaceRemove.java
@@ -0,0 +1,14 @@
+package StringAssignments;
+/*Remove all spaces in a String*/
+public class allSpaceRemove {
+
+	public static void main(String[] args) {
+		
+		String message = "    Hello    Everyone     " ;
+		
+		System.out.println(message.replace(" ", ""));
+		
+
+	}
+
+}
