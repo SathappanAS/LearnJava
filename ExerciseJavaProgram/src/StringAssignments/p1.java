dc5eeb41ed5d0aa19acbcec5c6b24d432ba5747c
sathappan1989 <sathappanas@outlook.com>
2025-02-01 22:26:24 -0700

    StringP1

---
+++ ExerciseJavaProgram/src/StringAssignments/p1.java
@@ -0,0 +1,16 @@
+package StringAssignments;
+/*Write a program to check two different strings equality*/
+public class p1 {
+
+	public static void main(String[] args) {
+		
+		String msgOne="Hello World";
+		String msgTwo="Hello World";
+		
+		boolean ans=msgOne.equals(msgTwo);
+		
+		System.out.println(ans);
+
+	}
+
+}
