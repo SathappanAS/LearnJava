8707044de21536efe550bbb071ff484cf27efb1b
sathappan1989 <sathappanas@outlook.com>
2025-02-01 23:05:23 -0700

    String SubString

---
+++ ExerciseJavaProgram/src/StringAssignments/subString.java
@@ -0,0 +1,12 @@
+package StringAssignments;
+
+public class subString {
+
+	public static void main(String[] args) {
+		String str="Hello World";
+		System.out.println(str.substring(0,5));
+		System.out.println(str.substring(6,11));
+
+	}
+
+}
