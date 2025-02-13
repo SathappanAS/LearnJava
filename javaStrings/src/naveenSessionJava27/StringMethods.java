724a0be7fdb91cabc3538d34be71ebf7f7a40533
sathappan1989 <sathappanas@outlook.com>
2025-02-12 18:43:19 -0700

    JavaStringLearning

---
+++ javaStrings/src/naveenSessionJava27/StringMethods.java
@@ -0,0 +1,18 @@
+package naveenSessionJava27;
+
+public class StringMethods {
+
+	public static void main(String[] args) {
+		
+		
+		String str="hi this is my java code and i am so happy";
+		
+		System.out.println(str.length());
+		int lenghtofStr=str.length();
+		
+		
+		
+
+	}
+
+}
