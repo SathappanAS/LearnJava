3fdc09bd07fb6138a493f720077bea6ed07bea86
sathappan1989 <sathappanas@outlook.com>
2025-02-19 06:35:19 -0700

    StringBuilderVsStringBuffer

--- Dec2024/src/StringManipulation/StringBuilderVsStringBuffer.java
+++ Dec2024/src/StringManipulation/StringBuilderVsStringBuffer.java
@@ -1,10 +1,30 @@
 package StringManipulation;
-
+/*Very Important Interview Question*/
 public class StringBuilderVsStringBuffer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		//Immutability - Immutable, once created cannot be changed
+		//Thread-Safe due to immutability
+		String s1="hello";
+		s1.concat(" Automation");
+		System.out.println(s1); //hello
+		System.out.println(s1.concat(" Automation")); //hello Automation
+		
+		//Mutable, content can be changed
+		//Not thread-safe, ideal for single-threaded scenarios
+		StringBuilder sb = new StringBuilder("hello ");
+		sb.append("Automation"); //helloautomation
+		System.out.println(sb);
+			
+		//Mutable, content can be changed
+		//Thread-safe, synchronized for multi-threaded scenarios
+		StringBuffer sbf=new StringBuffer("testing");
+		sbf.append(" selenium");
+		System.out.println(sbf); //testing selenium
+		
+		
 	}
 
 }
