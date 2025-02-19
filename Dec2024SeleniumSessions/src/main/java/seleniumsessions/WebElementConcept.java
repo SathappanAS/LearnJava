523efd21f55892d1373b27b34677fee1049befb2
sathappan1989 <sathappanas@outlook.com>
2025-02-18 20:41:06 -0700

    FindElement

--- Dec2024SeleniumSessions/src/main/java/seleniumsessions/WebElementConcept.java
+++ Dec2024SeleniumSessions/src/main/java/seleniumsessions/WebElementConcept.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebElementConcept {
 
+	static WebDriver driver;
+	
 	public static void main(String[] args) {
 		
 		WebDriver driver=new ChromeDriver();
@@ -19,15 +21,32 @@ public class WebElementConcept {
 		// driver.findElement(By.id("input-password")).sendKeys(By.id("input-password"));
 		
 		//2.
-		WebElement email=driver.findElement(By.id("input-email"));
-		WebElement password=driver.findElement(By.id("input-password"));
-		
-		email.sendKeys("tom@gmail.com");
-		password.sendKeys("tom@123");
+		//		WebElement email=driver.findElement(By.id("input-email"));
+		//		WebElement password=driver.findElement(By.id("input-password"));
+		//		
+		//		email.sendKeys("tom@gmail.com");
+		//		password.sendKeys("tom@123");
 		
 		//3. By locator:
+		//		By emailField = By.id("input-email");
+		//		By pwdField = By.id("input-password");
+		//		
+		//		WebElement email =driver.findElement(emailField);
+		//		WebElement passoword = driver.findElement(pwdField);
+		//		
+		//		email.sendKeys("tom@gmail.com");
+		//		passoword.sendKeys("tom@123");
+		
+		//4. By locator + generic function to get the element.
+		By emailField = By.id("input-email");
+		By pwdField= By.id("input-password");
+		
 		
 
+	}
+	
+	public static WebElement getElement(By locator) {
+		return driver.findElement(locator);
 	}
 
 }
