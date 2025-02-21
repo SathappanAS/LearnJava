ae0d8f1e5c6ee9d44410b7926a0364d10ea684d6
sathappan1989 <sathappanas@outlook.com>
2025-02-21 14:14:41 -0700

    StringRemoveSpaceInString

---
+++ javaStrings/src/naveenStringAssignments/removeSpaceInString.java
@@ -0,0 +1,8 @@
+package naveenStringAssignments;
+public class removeSpaceInString {
+	public static void main(String[] args) {	
+		String h1="  Hello  Everyone  ";
+		String h2=h1.replace(" ","");
+		System.out.println(h2);
+	}
+}
