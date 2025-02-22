96c11570e81f3d4c8b5b0670f048ad724288ab14
sathappan1989 <sathappanas@outlook.com>
2025-02-21 23:36:58 -0700

    selenium

---
+++ javaStrings/src/naveenStringAssignments/lastHalString.java
@@ -0,0 +1,15 @@
+package naveenStringAssignments;
+/*Write a program that gives you the last half of the string*/
+public class lastHalString {
+
+	public static void main(String[] args) {
+		
+		String statments = "My name is sathappan";
+		System.out.println(statments.length());
+		
+		String result = statments.substring(11, 20);
+		System.out.println(result);
+
+	}
+
+}
