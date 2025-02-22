4e40acf23c2df01c6779735270b7f02fe09579b1
sathappan1989 <sathappanas@outlook.com>
2025-02-21 23:28:02 -0700

    StringProgram

---
+++ javaStrings/src/naveenStringAssignments/StringReverseFunction.java
@@ -0,0 +1,16 @@
+package naveenStringAssignments;
+
+public class StringReverseFunction {
+
+	public static void main(String[] args) {
+		
+		StringBuilder b1 = new StringBuilder("sathappan");
+		b1.reverse();
+		System.out.println(b1);
+		
+		StringBuffer b2=new StringBuffer("ChennAI");
+		b2.reverse();
+		System.out.println(b2);
+	}
+
+}
