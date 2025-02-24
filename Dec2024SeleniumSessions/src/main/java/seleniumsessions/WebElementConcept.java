95ad5f8bfb2631f25ad9857b6e7bb2d689b49ac9
sathappan1989 <sathappanas@outlook.com>
2025-02-24 06:51:43 -0700

    Selenium

--- Dec2024SeleniumSessions/src/main/java/seleniumsessions/WebElementConcept.java
+++ Dec2024SeleniumSessions/src/main/java/seleniumsessions/WebElementConcept.java
@@ -11,42 +11,54 @@ public class WebElementConcept {
 	
 	public static void main(String[] args) {
 		
-		WebDriver driver=new ChromeDriver();
-		driver.get("https://naveenautomationlabs.app.box.com/notes/1720417859323");
+		WebElementConcept.driver=new ChromeDriver();
+		driver.get("https://naveenautomationlabs.com/opencart/index.php?route=account/login");
 		
 		//create a webElement(FE) + perform action(click,sendKeys, isDisplayed, getText..)
 		
-		//1.
+		// 1.
 		// driver.findElement(By.id("input-email")).sendKeys("tom@gmail.com");
 		// driver.findElement(By.id("input-password")).sendKeys(By.id("input-password"));
 		
-		//2.
-		//		WebElement email=driver.findElement(By.id("input-email"));
-		//		WebElement password=driver.findElement(By.id("input-password"));
-		//		
-		//		email.sendKeys("tom@gmail.com");
-		//		password.sendKeys("tom@123");
+		//2. (Reusability)
+		//WebElement email=driver.findElement(By.id("input-email"));
+		//WebElement password=driver.findElement(By.id("input-password"));
+			
+		//email.sendKeys("tom@gmail.com");
+		//password.sendKeys("tom@123");
 		
-		//3. By locator:
-		//		By emailField = By.id("input-email");
-		//		By pwdField = By.id("input-password");
-		//		
-		//		WebElement email =driver.findElement(emailField);
-		//		WebElement passoword = driver.findElement(pwdField);
-		//		
-		//		email.sendKeys("tom@gmail.com");
-		//		passoword.sendKeys("tom@123");
+		//3. By locator: (Reusability and no error)
+		//By emailField = By.id("input-email");
+		//By pwdField = By.id("input-password");
+		
+		
+		//WebElement email =driver.findElement(emailField);
+		//WebElement passoword = driver.findElement(pwdField);	
+		
+		//email.sendKeys("tom@gmail.com");
+		//passoword.sendKeys("tom@123");
 		
 		//4. By locator + generic function to get the element.
-		By emailField = By.id("input-email");
-		By pwdField= By.id("input-password");
+		//By emailField = By.id("input-email");
+		//By pwdField= By.id("input-password");
+		//		
+		//getElement(emailField).sendKeys("tom@gmail.com");
+		//getElement(pwdField).sendKeys("tom@123");
 		
+		//5.  By locator + generic function to get the element and perform actions.
+				By emailField = By.id("input-email");
+				By pwdField= By.id("input-password");
+				
+				doSendKeys(emailField,"tom@gmail.com");
+				doSendKeys(pwdField,"password");
 		
-
+	}
+	
+	public static void doSendKeys(By locator,String value) {
+		getElement(locator).sendKeys(value);
 	}
 	
 	public static WebElement getElement(By locator) {
 		return driver.findElement(locator);
 	}
-
 }
