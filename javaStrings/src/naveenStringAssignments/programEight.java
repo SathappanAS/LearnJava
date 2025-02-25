ca0d488d0f147798d2a62a1818bed27c2dc7caa0
sathappan1989 <sathappanas@outlook.com>
2025-02-25 13:46:54 -0700

    indexCheck

--- javaStrings/src/naveenStringAssignments/programEight.java
+++ javaStrings/src/naveenStringAssignments/programEight.java
@@ -2,13 +2,27 @@ package naveenStringAssignments;
 /*Write a method which gives an index of (-1) if string is not available. . it should return integer. if String is present, then it should return the specific index.*/
 public class programEight{
 
+	public int isStringPresent(String str , String searchStr)
+	{
+		int index=0;
+		if(str.contains(searchStr))
+		{
+			index=str.indexOf(searchStr);
+			return(index);
+		}
+		else
+		{
+			index=-1;
+			return index;
+		}
+		
+	}
+	
 	public static void main(String[] args) {
+		programEight f= new programEight();
+		System.out.println("Index of searched string is : "+ f.isStringPresent("Hello World", "W"));
 		
-			
-String str="My name is the sathappan";
-		
-		System.out.println(str.indexOf('z')); //-1
-		System.out.println(str.indexOf('a')); //4
+
 	}
 
 }
