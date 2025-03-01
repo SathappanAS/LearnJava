3ee033317ff861b9c7a46d60f852d2f1174f59bf
sathappan1989 <sathappanas@outlook.com>
2025-03-01 12:05:01 -0700

    naveenAutomation

--- Dec2024SeleniumSessions/src/main/java/seleniumsessions/ElementDisplayed.java
+++ Dec2024SeleniumSessions/src/main/java/seleniumsessions/ElementDisplayed.java
@@ -1,15 +1,12 @@
 package seleniumsessions;
 
-import java.util.NoSuchElementException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 public class ElementDisplayed {
-
-
 	
 	static WebDriver driver;
 	public static void main(String[] args) {
