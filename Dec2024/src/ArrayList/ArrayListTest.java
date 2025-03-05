869d5443802046c548546cad59c2fa07eb977bb2
sathappan1989 <sathappanas@outlook.com>
2025-03-04 22:35:41 -0700

    ArrayList

--- Dec2024/src/ArrayList/ArrayListTest.java
+++ Dec2024/src/ArrayList/ArrayListTest.java
@@ -6,28 +6,31 @@ public class ArrayListTest {
 
 	public static void main(String[] args) {
 		
-		//List(I) --- > ArrayList(C)
+		//List(Interface) --- > ArrayList(Child class)
 		//maintain the order/index
 		
-		ArrayList ar = new ArrayList();
-		//System.out.println(ar.get(0));
+		ArrayList ar = new ArrayList(); //vc=10; pc=0
+		//System.out.println(ar.get(0)); //IndexOutOfBoundException
 		
-		System.out.println(ar.size()); //0
+		System.out.println(ar.size()); //pc=0
 		System.out.println(ar);//[]
 		
-		ar.add(100);//0
-		ar.add(200);//1
-		ar.add(300);//2
+		ar.add(100);//0 location
+		ar.add(200);//1 location
+		ar.add(300);//2 location
 		
-		System.out.println(ar.get(0));
-		System.out.println(ar.size()); 
+		System.out.println(ar.get(0)); //100
+		System.out.println(ar.size());  //size will be 3
 		
 		ar.add(400);//1
 		ar.add(500);//2
-		
+		System.out.println(ar); //[100, 200, 300, 400, 500]
+		System.out.println(ar.size());  //5
 		ar.remove(3);
-		System.out.println(ar.size());
+		 
+		System.out.println(ar.size());	//4
+		System.out.println(ar); // [100, 200, 300, 500]
+		System.out.println(ar.get(3)); //500
 		
 	}
-
 }
