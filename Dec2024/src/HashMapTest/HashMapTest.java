3d14ad91b5f2cac0a9190e85603e12c8bb377fc4
sathappan1989 <sathappanas@outlook.com>
2025-03-05 22:50:22 -0700

    HashMap

--- Dec2024/src/HashMapTest/HashMapTest.java
+++ Dec2024/src/HashMapTest/HashMapTest.java
@@ -18,35 +18,58 @@ public class HashMapTest {
 		markMap.put("jyothi",100);
 		markMap.put("aparna",60);
 		
-		System.out.println(markMap.size());
+		System.out.println(markMap.size()); //2
 		
 		markMap.put("dev",75);
-		System.out.println(markMap.size());
+		System.out.println(markMap.size()); //3
 		markMap.put("dev",45);
-		System.out.println(markMap.get("dev"));
+		System.out.println(markMap.get("dev")); //45
 		System.out.println("---");
-		System.out.println(markMap.get(75));
+		System.out.println(markMap.get(75));//null
 		
 		markMap.put("naveen",45);
 		
-		System.out.println(markMap.size());
+		System.out.println(markMap.size()); //4
 		
-		System.out.println(markMap.get("dev"));
+		System.out.println(markMap.get("dev")); //45
 		System.out.println(markMap.get("tom"));//null
 		
 		markMap.put(null,55);
-		System.out.println(markMap.get(null));
+		System.out.println(markMap.get(null));//55
 		
 		markMap.put(null,15);
-		System.out.println(markMap.get(null));
+		System.out.println(markMap.get(null));//15
 		
 		markMap.put("robin",null);
 		markMap.put("divya",null);
-		System.out.println(markMap.get("divya"));
+		System.out.println(markMap.get("divya")); //null
 		
 		
 		//
 		
+		HashMap<Integer, Integer> numMap=new HashMap<Integer, Integer>();
+		numMap.put(100, 1);
+		
+		
+		HashMap<String, String> empMap=new HashMap<String, String>();
+		empMap.put("ravi", "SDET1'");
+		empMap.put("ravi", "SDET1");
+		empMap.put("dev", "SDET2");
+		empMap.put("jyothi", "SDET Manager");
+		empMap.put("divya", "dev Manager");
+		empMap.put("naveen", "CTO");
+		empMap.put(null, "CEO");
+		
+		System.out.println(empMap);
+		//{null=CEO, dev=SDET2, ravi=SDET1, naveen=CTO, jyothi=SDET Manager, divya=dev Manager}
+		System.out.println(empMap.size()); //6
+		
+		
+		HashMap<String, Object> userMap=new HashMap<String, Object>();
+		userMap.put("tom", "IBM");
+		userMap.put("designation", "SDET");
+		userMap.put("salary", 14.55);
+		userMap.put("isActive", true);
 		
 		
 		
