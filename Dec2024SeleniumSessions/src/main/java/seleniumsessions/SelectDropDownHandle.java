924dafd11aef415fb4ba1d9a865e403f7129d25d
sathappan1989 <sathappanas@outlook.com>
2025-03-22 21:02:33 -0700

    DropDown

---
+++ Dec2024SeleniumSessions/src/main/java/seleniumsessions/SelectDropDownHandle.java
@@ -0,0 +1,15 @@
+package seleniumsessions;
+
+public class SelectDropDownHandle {
+
+	public static void main(String[] args) {
+		
+		//dropdown:
+		//htmltag: <select>--> select class -- default class in selenium
+		//select --  options tag
+		//option : value = ,text = 
+		
+
+	}
+
+}
