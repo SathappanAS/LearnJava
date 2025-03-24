4306236b229b08a6b9803ab0824ba989afecd232
sathappan1989 <sathappanas@outlook.com>
2025-03-23 23:53:11 -0700

    Selenium

--- seleniumpractice/src/main/java/session13/circinfo.java
+++ seleniumpractice/src/main/java/session13/circinfo.java
@@ -1,7 +1,10 @@
 package session13;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class circinfo {
@@ -14,12 +17,35 @@ public class circinfo {
 		driver.get(
 				"https://www.espncricinfo.com/series/ipl-2025-1449924/chennai-super-kings-vs-mumbai-indians-3rd-match-1473440/full-scorecard");
 		
-		String wktTakerName = driver.findElement(By.xpath("//span[text()='Ryan Rickelton']/ancestor::td/following-sibling::td")).getText();
-		System.out.println(wktTakerName);
+//		String wktTakerName = driver.findElement(By.xpath("//span[text()='Ryan Rickelton']/ancestor::td/following-sibling::td")).getText();
+//		System.out.println(wktTakerName);
+	
+		
+		getWktTakerName("Ryan Rickelton"); //b Ahmed
+		getWktTakerName("MS Dhoni"); // not out
+		
 		Thread.sleep(4000);
-
+		
+		//command+shift+2
+		List<WebElement> scoreList = driver.findElements(By.xpath("//span[text()='Rachin Ravindra']/ancestor::td/following-sibling::td[contains(@class, 'ds-text-right')]"));
+		System.out.println(scoreList.size());
+		
+		for(WebElement e : scoreList) {
+			String text=e.getText();
+			System.out.println(text);
+		}
+		
 		driver.quit();
 	}
+	
+	
+	public static void getWktTakerName (String playerName) {
+		String wktTakerName = driver.findElement(By.xpath("//span[text()='"+playerName+"']/ancestor::td/following-sibling::td")).getText();
+		System.out.println(wktTakerName);
+	}
+	
 
+
+	
 }
   
