d90781953e77cd9accfe7efd2e9c3db9346ce276
sathappan1989 <sathappanas@outlook.com>
2025-03-24 00:02:27 -0700

    cricinfo.java

--- seleniumpractice/src/main/java/session13/circinfo.java
+++ seleniumpractice/src/main/java/session13/circinfo.java
@@ -1,5 +1,6 @@
 package session13;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -26,25 +27,38 @@ public class circinfo {
 		
 		Thread.sleep(4000);
 		
-		//command+shift+2
-		List<WebElement> scoreList = driver.findElements(By.xpath("//span[text()='Rachin Ravindra']/ancestor::td/following-sibling::td[contains(@class, 'ds-text-right')]"));
-		System.out.println(scoreList.size());
-		
-		for(WebElement e : scoreList) {
-			String text=e.getText();
-			System.out.println(text);
-		}
+//			List<WebElement> scoreList = driver.findElements(By.xpath("//span[text()='Rachin Ravindra']/ancestor::td/following-sibling::td[contains(@class, 'ds-text-right')]"));
+//			System.out.println(scoreList.size());
+//			
+//			for(WebElement e : scoreList) {
+//				String text=e.getText();
+//				System.out.println(text);
+//			}
 		
 		driver.quit();
 	}
 	
 	
-	public static void getWktTakerName (String playerName) {
+	public static String getWktTakerName (String playerName) {
 		String wktTakerName = driver.findElement(By.xpath("//span[text()='"+playerName+"']/ancestor::td/following-sibling::td")).getText();
 		System.out.println(wktTakerName);
+		return wktTakerName;
 	}
 	
 
+	public static List<String> getScoreCard (String playerName) {
+		List<WebElement> scoreList = driver.findElements(By.xpath("//span[text()='"+playerName+"']/ancestor::td/following-sibling::td[contains(@class, 'ds-text-right')]"));
+		System.out.println(scoreList.size());
+		List<String> scoreCardList = new ArrayList<String>();//[]
+		for(WebElement e : scoreList) {
+			String text=e.getText();
+			System.out.println(text);
+			scoreCardList.add(text);
+		}
+		return scoreCardList;
+	}
+	
+	
 
 	
 }
